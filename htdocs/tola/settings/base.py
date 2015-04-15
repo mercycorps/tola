3aba8d792aec915474a2f21f67e640aeca4cb9a3
Mahmood Khan <mkhan@mercycorps.org>
2015-04-14 17:41:24 -0700

    added AjaxMessaging middleware to base settings file.

--- htdocs/tola/settings/base.py
+++ htdocs/tola/settings/base.py
@@ -163,6 +163,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'tola.middleware.AjaxMessaging.AjaxMessaging',
 )
 ########## END MIDDLEWARE CONFIGURATION
 
