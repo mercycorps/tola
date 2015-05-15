b03f9ac298c2fb486ebac4a35a80bd74f3450b2d
Mahmood Khan <mkhan@mercycorps.org>
2015-05-15 11:29:42 -0700

    made edit and del buttons get proper link using mongo _id field

--- htdocs/silo/views.py
+++ htdocs/silo/views.py
@@ -352,21 +352,24 @@ def viewSilo(request,id):
 
     return render(request, 'display/silo-sources.html',{'get_sources':get_sources})
 
-
-
+        
 def define_table(columns):
+    from django.template.base import add_to_builtins
+    add_to_builtins('silo.templatetags.underscoretags')
+    
     """
     Dynamically builds a django-tables2 table without specifying the column names
     It is important to build the django-tables2 dynamically because each time a silo 
     is loaded from MongoDB, it is not known what columns heading it has or how mnay columns it has
     """
     EDIT_DEL_TEMPLATE = '''
-        <a class="btn btn-default btn-xs" role="button" href="/value_edit/{{ record.id }}">Edit</a>
-        <a class="btn btn-danger btn-xs" role="button" href="/value_delete/{{ record.id }}">Delete</button> 
+        <a class="btn btn-default btn-xs" role="button" href="/value_edit/{{ record|get:'_id'|get:'$oid' }}">Edit</a>
+        <a class="btn btn-danger btn-xs" style="color: #FFF;" role="button" href="/value_delete/{{ record|get:'_id'|get:'$oid'  }}">Delete</button> 
         '''
     attrs = dict((c, tables.Column()) for c in columns)
     attrs['Operation'] = tables.TemplateColumn(EDIT_DEL_TEMPLATE)
-    attrs['Meta'] = type('Meta', (), dict(attrs={"class":"paleblue", "orderable":"True", "width":"100%"}) )
+    attrs['Meta'] = type('Meta', (), dict(exclude=["_id"], attrs={"class":"paleblue", "orderable":"True", "width":"100%"}) )
+    
     klass = type('DynamicTable', (tables.Table,), attrs)
     return klass
 
@@ -376,7 +379,7 @@ def siloDetail(request,id):
     """
     Show silo source details
     """
-    table = LabelValueStore.objects(silo_id=id).exclude("id").to_json()
+    table = LabelValueStore.objects(silo_id=id).to_json()
     decoded_json = json.loads(table)
     silo = define_table(decoded_json[0].keys())(decoded_json)
     
