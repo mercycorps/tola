954cfe1221e77c6982a9b110e04e05599336610a
Mahmood Khan <mkhan@mercycorps.org>
2015-06-26 12:02:47 -0700

    added unittests to test csv file upload

--- htdocs/silo/test.py
+++ htdocs/silo/test.py
@@ -4,12 +4,14 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.urlresolvers import resolve, reverse
+from django.template.loader import render_to_string
 
 from django.test import TestCase
 from django.test import Client
 from django.test import RequestFactory
-from django.core.urlresolvers import resolve, reverse
-from django.template.loader import render_to_string
+
 from silo.views import *
 from silo.models import *
 
@@ -23,11 +25,8 @@ class ReadTest(TestCase):
         self.factory = RequestFactory()
         self.user = User.objects.create_user(username="joe", email="joe@email.com", password="tola123")
     
-    def test_login(self):
-        response = self.client.post('/accounts/login/', {'username': 'jj', 'password': 'kkk'})
-        self.assertEqual(response.status_code, 200)
-    
     def test_new_read_get(self):
+        """ Test the GET view of initRead view """
         request = self.factory.get(self.new_read_url)
         request.user = self.user
         response = initRead(request)
@@ -35,6 +34,7 @@ class ReadTest(TestCase):
     
     def test_new_read_post(self):
         read_type = ReadType.objects.get(read_type="JSON")
+        upload_file = open('test.csv', 'rb')
         params = {
             'owner': self.user.pk,
             'type': read_type.pk,
@@ -43,6 +43,7 @@ class ReadTest(TestCase):
             'read_url': 'https://www.lclark.edu',
             'resource_id':'testsssszzz',
             'create_date': '2015-06-24 20:33:47',
+            'name': upload_file,
         }
         request = self.factory.post(self.new_read_url, data = params)
         request.user = self.user
@@ -62,9 +63,13 @@ class ReadTest(TestCase):
         response = self.client.get(self.show_read_url + str(source.pk) + "/")
         self.assertEqual(response.status_code, 200)
 
-class SiloTest(TestCase):
 
-    silo_detail_url = "/silo_edit/"
+class SiloTest(TestCase):
+    fixtures = ['fixtures/read_types.json']
+    silo_edit_url = "/silo_edit/"
+    upload_csv_url = "/file/"
+    silo_detail_url = "/silo_detail/"
+    
     def setUp(self):
         self.client = Client()
         self.factory = RequestFactory()
@@ -79,7 +84,7 @@ class SiloTest(TestCase):
         self.assertEqual(silo.pk, 1)
         
         # Fetch the silo that just got created above
-        request = self.factory.get(self.silo_detail_url)
+        request = self.factory.get(self.silo_edit_url)
         request.user = self.user
         response = editSilo(request, silo.pk)
         self.assertEqual(response.status_code, 200)
@@ -90,19 +95,49 @@ class SiloTest(TestCase):
             'name': 'Test Silo Updated',
             'description': 'Adding this description in a unit-test.',
         }
-        request = self.factory.post(self.silo_detail_url, data = params)
+        request = self.factory.post(self.silo_edit_url, data = params)
         request.user = self.user
         request._dont_enforce_csrf_checks = True
         response = editSilo(request, silo.pk)
         if response.status_code == 302:
-            print("Redirecting")
             self.assertEqual(response.url, "/display")
         else:
             self.assertEqual(response.status_code, 200)
         
     def test_new_silodata(self):
-        pass
+        read_type = ReadType.objects.get(read_type="CSV")
+        upload_file = open('test.csv', 'rb')
+        read = Read.objects.create(owner=self.user, type=read_type, 
+            read_name="TEST CSV IMPORT", description="unittest", create_date='2015-06-24 20:33:47', 
+            file_data=SimpleUploadedFile(upload_file.name, upload_file.read())
+        )
+        params =  {
+            "read_id": read.pk,
+            "new_silo": "Test CSV Import",
+        }
+        request = self.factory.post(self.upload_csv_url, data = params)
+        request.user = self.user
+        request._dont_enforce_csrf_checks = True
+        response = uploadFile(request, read.pk)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/silo_detail/", response.url)
+        
+        silo = Silo.objects.get(name="Test CSV Import")
+        request = self.factory.get(self.silo_detail_url)
+        request.user = self.user
+        
+        response = siloDetail(request, silo.pk)
+        self.assertEqual(response.status_code, 200)
+        
+        
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
     
+    def test_read_form(self):
+        upload_file = open('test.csv', 'rb')
+        file_dict = {'file': SimpleUploadedFile(upload_file.name, upload_file.read())}
+        
     def test_delete_data_from_silodata(self):
         pass
     
