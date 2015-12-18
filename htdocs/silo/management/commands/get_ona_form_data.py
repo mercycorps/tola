d322d486da4e3edc558c7eba7a9a3c3abb0573bb
Mahmood Khan <mkhan@mercycorps.org>
2015-12-18 15:23:58 -0800

    fetches ona form data and stores it in a silo

--- htdocs/silo/management/commands/get_ona_form_data.py
+++ htdocs/silo/management/commands/get_ona_form_data.py
@@ -1,13 +1,17 @@
-import json
-
-from django.core.management.base import BaseCommand, CommandError
-from silo.models import *
-
-import requests
+import requests, json
 from requests.auth import HTTPDigestAuth
 
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+from silo.models import LabelValueStore, Read, Silo, ThirdPartyTokens
+from tola.util import siloToDict, combineColumns
 
 class Command(BaseCommand):
+    """
+    Usage: python manage.py get_ona_form_data --username mkhan --read_ids 2 9 --silo_id 1
+    """
     help = 'Fetches a specific form data from ONA'
 
     def add_arguments(self, parser):
@@ -32,13 +36,30 @@ class Command(BaseCommand):
             try:
                 read = reads.filter(pk=read_id)[0]
             except Read.DoesNotExist:
-                raise CommandError('Read "%s" does not exist' % read_id)
+                raise CommandError('Read "%s" does not exist for user, %s' % (read_id, user.username))
 
             # Fetch the data from ONA
             ona_token = ThirdPartyTokens.objects.get(user=user.pk, name="ONA")
             response = requests.get(read.read_url, headers={'Authorization': 'Token %s' % ona_token.token})
-            #data = json.loads(response.content)
-            # now fetch the data from ONA and put it into a silo
-            #self.stdout.write("read %s" % read_id, ending='')
+            data = json.loads(response.content)
+
+            # import data into this silo
+            num_rows = len(data)
+            if num_rows == 0:
+                continue
+
+            counter = None
+            #loop over data and insert create and edit dates and append to dict
+            for counter, row in enumerate(data):
+                lvs = LabelValueStore()
+                lvs.silo_id = silo.pk
+                for new_label, new_value in row.iteritems():
+                    if new_label is not "" and new_label is not None and new_label is not "edit_date" and new_label is not "create_date":
+                        setattr(lvs, new_label, new_value)
+                lvs.create_date = timezone.now()
+                result = lvs.save()
+
+            if num_rows == (counter+1):
+                combineColumns(silo_id)
+
             self.stdout.write('Successfully fetched the READ_ID, "%s", from database' % read_id)
-            self.stdout.write(str(response.content))
