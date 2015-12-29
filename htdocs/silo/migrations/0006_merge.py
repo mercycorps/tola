aa660117916aaa062b566a0ecf0128d4473d9fdb
root <root@neikea.mercycorps.org>
2015-12-28 16:30:09 -0800

    merge migration conflicts

---
+++ htdocs/silo/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('silo', '0004_auto_20151014_1122'),
+        ('silo', '0005_auto_20151014_1539'),
+    ]
+
+    operations = [
+    ]
